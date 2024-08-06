86fb335d457da2f9b35f3c26ceb1aca0cbcbfb3f
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-06 17:41:43 +0200

    commit
    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Main Section
+      </h1>
+    </div>
+
   )
 }
