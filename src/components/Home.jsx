a309e154b5507bf6be812823c2b2328d9c649cb4
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-11 01:01:22 +0200

    del
    remove navbar add to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function Home() {
     return (
         <div className='text-4xl font-semibold '>
-            <Navbar />
+            <FontAwesomeIcon icon={faBars} className='text-6xl' />
         </div>
     )
 }
