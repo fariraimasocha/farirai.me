4ce5b918ba0e738bf80b42aa7f1a9acf4085c00c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-12 21:54:14 +0200

    fix
    hover on companies

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,9 +1,12 @@
-
 import React from 'react';
 
-function Company({ companyName, project, field, presentation, bgClass }) {
+function Company({ companyName, project, field, presentation, onHover, onHoverOut }) {
     return (
-        <div className={`flex justify-between px-20 border-b-2 border-pink py-40 transition hover:${bgClass}`}>
+        <div
+            className={`flex justify-between px-20 border-b-2 border-pink py-40 transition`}
+            onMouseEnter={onHover}
+            onMouseLeave={onHoverOut}
+        >
             <div>
                 <h1 className='text-8xl font-black hover:text-darky1 transition'>
                     {companyName}
@@ -15,7 +18,7 @@ function Company({ companyName, project, field, presentation, bgClass }) {
             <div className='tracking-widest'>
                 <h1 className='text-2xl font-semibold'>
                     {field}
-                </h1>
+                </h1 >
                 <p className='text-2xl font-semibold mt-1'>
                     {presentation}
                 </p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Company from './Company';
 
 function Projects() {
+    const [bgClass, setBgClass] = useState('bg-blue-300');
+
     return (
-        <div className=' bg-pin transition'>
+        <div className={`transition ${bgClass}`}>
             <div className='flex space-x-5 mb-4 justify-center mx-auto text-7xl py-20 hover:animate-pulse'>
                 <h2 className='font-light'>FEATURED</h2>
                 <h2 className='font-light mt-10'>PROJECTS</h2>
             </div>
-            <Company
-                companyName="Flixtechs"
-                project="Contractease website"
-                field="WEB DEVELOPMENT"
-                presentation="WEBSITE"
-                bgClass="bg-yel"
-            />
-            <Company
-                companyName="Codecrafters"
-                project="Livestock tracking system"
-                field="IOT"
-                presentation="PRODUCT"
-                bgClass="bg-chel"
-            />
-            <Company
-                companyName="Tirelo Capital"
-                project="Satechi E Learning website"
-                field="WEB DESIGN"
-                presentation="WEBSITE"
-                bgClass="bg-peach"
-            />
+            <div className='justify-center mx-auto w-11/12'>
+                <Company
+                    companyName="Flixtechs"
+                    project="Contractease website"
+                    field="WEB DEVELOPMENT"
+                    presentation="WEBSITE"
+                    onHover={() => setBgClass('bg-yellow-500')}
+                    onHoverOut={() => setBgClass('bg-blue-300')}
+                />
+                <Company
+                    companyName="Codecrafters"
+                    project="Livestock tracking system"
+                    field="IOT"
+                    presentation="PRODUCT"
+                    onHover={() => setBgClass('bg-red-500')}
+                    onHoverOut={() => setBgClass('bg-blue-300')}
+                />
+                <Company
+                    companyName="Tirelo Capital"
+                    project="Satechi E Learning website"
+                    field="WEB DESIGN"
+                    presentation="WEBSITE"
+                    onHover={() => setBgClass('bg-pink-500')}
+                    onHoverOut={() => setBgClass('bg-blue-300')}
+                />
+            </div>
             <div className='flex items-center justify-center px-20 border-b hover:bg-darky py-40 transition'>
                 <a href="#">
                     <button className='px-6 py-2 rounded-3xl ring-2 ring-darky1 text-darky1 font-semibold text-2xl hover:bg-black hover:text-white'>All Work</button>
