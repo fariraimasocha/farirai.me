8844177e4b9a0d3163c9e58259c54b040513802d
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-13 19:01:59 +0200

    wip
    work on all projects page

--- src/components/AllWork.jsx
+++ src/components/AllWork.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NavModal from './NavModal'
 
 function AllWork() {
     return (
-        <div>AllWork</div>
+        <div className='min-h-screen flex flex-col'>
+            <NavModal />
+            <div className='bg-blue-300 mt-20 min-h-screen'>
+                <div className="flex flex-col md:flex-row md:space-x-5 mb-4 justify-center mx-auto text-4xl md:text-7xl py-10 md:py-20 hover:animate-pulse bg-blue-300">
+                    <h2 className="font-light text-center md:text-left">ALL</h2>
+                    <h2 className="font-light mt-5 md:mt-10 text-center md:text-left">WORK</h2>
+                </div>
+                <div className='flex space-x-5'>
+
+                </div>
+            </div>
+
+        </div>
     )
 }
 
