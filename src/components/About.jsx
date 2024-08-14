17f684cbd8886f28b13d1f75f266bbce0bb0db44
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-14 10:11:02 +0200

    wip
    feat finish about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavModal from './NavModal'
 import { Link } from 'react-router-dom'
+import CompanyCard from './CompanyCard'
+
 function About() {
     return (
         <div className='flex flex-col bg-lighty'>
@@ -35,10 +37,40 @@ function About() {
                             My Linkedln
                         </p>
                     </Link>
-
-
                 </div>
             </div>
+            <div className='bg-blue-300 mt-20 min-h-screen'>
+                <div className="flex flex-col md:flex-row md:space-x-5 mb-4 justify-center mx-auto mt-10 text-4xl md:text-7xl py-10 md:py-20 hover:animate-pulse bg-blue-300">
+                    <h2 className="font-light text-center md:text-left">ALL</h2>
+                    <h2 className="font-light mt-5 md:mt-10 text-center md:text-left">WORK</h2>
+                </div>
+
+                <div className='flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 px-4 md:px-16 py-10'>
+
+                    <CompanyCard
+                        field="website"
+                        presentation="visual"
+                        imageSrc="images/favicon/favicon.png"
+                        companyName="Flixtechs"
+                        description="Contractease website"
+                        mt="" />
+                    <CompanyCard
+                        field="iot tag"
+                        presentation="visual"
+                        imageSrc="images/favicon/favicon.png"
+                        companyName="Coderafters"
+                        description="Livestock Antitheft System"
+                        mt="" />
+
+                </div>
+
+                <div className='flex items-center justify-center px-20 border-b py-20 transition'>
+                    <Link to="/all-work">
+                        <button className='px-6 py-2 rounded-3xl ring-2 ring-darky1 text-darky1 font-semibold text-2xl hover:bg-black hover:text-white'>All Work</button>
+                    </Link>
+                </div>
+            </div>
+
         </div>
     )
 }
