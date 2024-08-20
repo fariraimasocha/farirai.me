8c145808a93ef38bec0d3d3e7317587d803bbc93
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-20 19:20:10 +0200

    wip
    change project nav

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,10 +17,10 @@ const Card = ({ title, description, imageSrc, link }) => {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{title}</h5>
                 <p className="mb-3 font-normal text-gray-700">{description}</p>
                 <button
-                    className="inline-flex space-x-3 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                    className="inline-flex space-x-3 items-center px-5 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
                     onClick={handleClick}
                 >
-                    <h1>Details</h1>
+                    <h1 className='tracking-wider'>Visit</h1>
                     <FontAwesomeIcon icon={faArrowRight} />
                 </button>
             </div>
