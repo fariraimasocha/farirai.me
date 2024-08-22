b99dbe0535cd74fb565a267cff87ae77ae36f349
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-22 21:59:50 +0200

    fix
    center fa icon

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function SkillCard({ icon }) {
     return (
         <div className='rounded-xl border w-3/12'>
-            <div className='justify-center mx-auto'>
-                <FontAwesomeIcon icon={icon} className='text-3xl py-7' />
+            <div className='justify-center mx-auto text-center'>
+                <FontAwesomeIcon icon={icon} className='text-5xl py-7' />
             </div>
         </div>
 
