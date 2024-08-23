ad70817d4e8e7b428a4edd32435296db58477a03
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-23 15:50:13 +0200

    wip
    improve framer animations scale and rotate

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 function SkillCard({ icon, color, backgroundColor = '', textColor = 'text-gray-700' }) {
     return (
         <motion.div
-            whileHover={{ scale: 1.3, rotate: 5, translateY: -5, translateX: -5 }}
+            whileHover={{ scale: 2, rotate: 7, translateY: -5, translateX: -5 }}
             className={`rounded-xl ${backgroundColor}transition-shadow duration-300 ease-in-out`}
         >
             <div className='justify-center mx-auto text-center'>
