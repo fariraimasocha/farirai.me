3c63f0776e8afef03dab3bc521444d082e653073
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-23 13:42:53 +0200

    wip
    add transalte y logic skillcard

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 function SkillCard({ icon, color, backgroundColor = 'bg-white', textColor = 'text-gray-700' }) {
     return (
         <motion.div
-            whileHover={{ scale: 1.3, rotate: 5, translateY: -5 }}
+            whileHover={{ scale: 1.3, rotate: 5, translateY: -5, translateX: -5 }}
             className={`rounded-xl border w-full sm:w-5/12 md:w-2/6 lg:w-1/5 p-6 sm:p-4 ${backgroundColor} shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out`}
         >
             <div className='justify-center mx-auto text-center'>
