eda8d3e66d085bbe0ba754ffa4e2d8e8af6f2151
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-08-23 15:37:53 +0200

    wip
    clean skill card section

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { motion } from 'framer-motion';
 
-function SkillCard({ icon, color, backgroundColor = 'bg-white', textColor = 'text-gray-700' }) {
+function SkillCard({ icon, color, backgroundColor = '', textColor = 'text-gray-700' }) {
     return (
         <motion.div
             whileHover={{ scale: 1.3, rotate: 5, translateY: -5, translateX: -5 }}
-            className={`rounded-xl border w-full sm:w-5/12 md:w-2/6 lg:w-1/5 p-6 sm:p-4 ${backgroundColor} shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out`}
+            className={`rounded-xl ${backgroundColor}transition-shadow duration-300 ease-in-out`}
         >
             <div className='justify-center mx-auto text-center'>
-                <FontAwesomeIcon icon={icon} className={`text-5xl py-4 ${textColor}`} style={{ color }} />
+                <FontAwesomeIcon icon={icon} className={`text-5xl ${textColor}`} style={{ color }} />
             </div>
         </motion.div>
     );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,20 +11,73 @@ const Skills = () => {
                 whileInView="visible"
                 viewport={{ once: true }}
                 className="text-5xl sm:text-6xl md:text-7xl font-bold text-gray-600 text-center mt-14 mb-20">Skills</motion.h2>
-            <div className='flex flex-wrap justify-center gap-5'>
-                <SkillCard icon={faReact} color='#61DAFB' />
-                <SkillCard icon={faHtml5} color='#E34F26' />
-                <SkillCard icon={faCss3} color='#1572B6' />
-                <SkillCard icon={faNodeJs} color='#68A063' />
-                <SkillCard icon={faAngular} color='#DD0031' />
-                <SkillCard icon={faLaravel} color='#FF2D20' />
+            <div className='flex flex-wrap justify-center gap-10'>
                 <SkillCard icon={faPython} color='#3776AB' />
                 <SkillCard icon={faPhp} color='#777BB4' />
                 <SkillCard icon={faSass} color='#CC6699' />
                 <SkillCard icon={faJs} color='#F7DF1E' />
                 <SkillCard icon={faLinux} color='#FCC624' />
                 <SkillCard icon={faLinode} color='#00A95C' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faReact} color='#61DAFB' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
+                <SkillCard icon={faLaravel} color='#FF2D20' />
+                <SkillCard icon={faHtml5} color='#E34F26' />
+                <SkillCard icon={faCss3} color='#1572B6' />
+                <SkillCard icon={faNodeJs} color='#68A063' />
+                <SkillCard icon={faAngular} color='#DD0031' />
             </div>
+
         </div>
     );
 };
